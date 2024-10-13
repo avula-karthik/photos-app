871bdc6a792cf49b1a187791c20b2af66b874c48
Karthik Avula <96226768+avula-karthik@users.noreply.github.com>
2024-10-13 20:34:51 +0530

    String Appended
    String appended to header for testing purpose

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import './App.css';
-import Photos from './Photos';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Photos from "./Photos";
+
+function getRandomUppercaseLetter() {
+  const asciiCode = Math.floor(Math.random() * 26) + 65;
+  return String.fromCharCode(asciiCode);
+}
 
 function App() {
-    return (
-        <div className='App'>
-            <h1>
-                <b>Welcome to Gallery App</b>
-            </h1>
-            <Photos />
-        </div>
-    );
+  let [randomString, setRandomString] = useState("");
+  useEffect(() => {
+    setRandomString(getRandomUppercaseLetter());
+  }, []);
+  return (
+    <div className="App">
+      <h1>
+        <b>Welcome to Gallery App - server #{randomString}</b>
+        {/* refresh makes it fetch new letter everytime */}
+      </h1>
+      <Photos />
+    </div>
+  );
 }
 
 export default App;
