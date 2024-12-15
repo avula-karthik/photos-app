4888ffa78eae7f1758c1924c9712a497c93fdc24
Karthik Avula <96226768+avula-karthik@users.noreply.github.com>
2024-12-15 10:21:56 +0530

    undo

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   return (
     <div className="App">
       <h1>
-        <b>Welcome to Gallery App ! - server #{randomString} xDD</b>
+        <b>Welcome to Gallery App ! - server #{randomString}</b>
         {/* refresh makes it fetch new letter everytime */}
       </h1>
       <Photos />
